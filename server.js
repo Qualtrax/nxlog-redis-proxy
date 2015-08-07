66b2da7a6677427108609ae4e8c917bec1642ef6
Lucas Rose <lrose@qualtrax.com>
2015-08-07 13:23:44 -0400

    Working proxy to pass data from nxlog to redis

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var nconf = require('nconf');
+var express = require('express');
+var bodyParser = require('body-parser');
+var redis = require('redis');
+
+nconf.argv();
+nconf.defaults({
+  "host": "127.0.0.1"
+  "port": "8000", 
+  "redis-host": "127.0.0.1",
+  "redis-port": "6379",
+});
+
+var client = redis.createClient(nconf.get("redis-port"), nconf.get("redis-host"));
+var app = express();
+
+app.use(bodyParser.text());
+
+client.on("error", function(error) {
+  console.log("Error " + error);
+});
+
+app.post('/:queue', function (req, res) {
+  client.rpush(req.params.queue, req.body);
+  res.status(200).end();
+});
+
+app.on('close', function() {
+  client.quit();
+});
+
+app.listen(nconf.get("port"), nconf.get("host"));
